22d1020f60041d59e638d0e6c71040d691ae1294
Tin Nguyen <vietnguyen0425@gmail.com>
2025-04-12 17:27:59 -0400

    Updated App.tsx and tried to fix up HomeScreen, ReminderScreen, and ConfirmationScreen

---
+++ frontend/components/ConfirmationScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type RootStackParamList = {
+  Home: undefined;
+  Reminder: undefined;
+  Confirmation: undefined;
+};
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function ConfirmationScreen({ navigation }: Props) {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Welcome to MedReminder</Text>
+      <Button title="Go to Reminder" onPress={() => navigation.navigate('Reminder')} />
+    </View>
+  );
+}
